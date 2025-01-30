9c9e9eefd5794adb332c1195baee34148b409da2
drugsonwarsol <drugsonwarsol@hotmail.com>
2025-01-29 19:10:27 -0800

    attempt to fix es-lint errors with netlify

--- _lib/getXataClient.ts
+++ _lib/getXataClient.ts
@@ -1,16 +1,41 @@
-/* eslint-disable */
 import { XataClient } from "./xata.codegen";
-import fetch from "node-fetch";
 
 let instance: XataClient | undefined = undefined;
 
 export const getXataClient = () => {
   if (instance) return instance;
+  
+  const customFetch = typeof fetch === 'function' 
+    ? fetch 
+    : (url: string, init?: RequestInit) => {
+        const https = require('https');
+        return new Promise((resolve, reject) => {
+          const req = https.request(url, {
+            method: init?.method || 'GET',
+            headers: init?.headers,
+          }, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => resolve({
+              ok: res.statusCode >= 200 && res.statusCode < 300,
+              status: res.statusCode,
+              json: () => Promise.resolve(JSON.parse(data)),
+              text: () => Promise.resolve(data),
+            }));
+          });
+          
+          req.on('error', reject);
+          
+          if (init?.body) {
+            req.write(init.body);
+          }
+          req.end();
+        });
+      };
 
   instance = new XataClient({ 
-    apiKey: process.env.XATA_API_KEY,
-    branch: process.env.XATA_BRANCH || 'main',
-    //@ts-ignore
-    fetch });
+    fetch: customFetch as any
+  });
+  
   return instance;
 };
