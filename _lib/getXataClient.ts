46c63fa1933d55977c9b12fe367b0286b25b6cc5
drugsonwarsol <drugsonwarsol@hotmail.com>
2025-01-29 19:15:16 -0800

    oopsies, add missing variables

--- _lib/getXataClient.ts
+++ _lib/getXataClient.ts
@@ -5,35 +5,36 @@ let instance: XataClient | undefined = undefined;
 export const getXataClient = () => {
   if (instance) return instance;
   
-  const customFetch = typeof fetch === 'function' 
-    ? fetch 
-    : (url: string, init?: RequestInit) => {
-        const https = require('https');
-        return new Promise((resolve, reject) => {
-          const req = https.request(url, {
-            method: init?.method || 'GET',
-            headers: init?.headers,
-          }, (res) => {
-            let data = '';
-            res.on('data', chunk => data += chunk);
-            res.on('end', () => resolve({
-              ok: res.statusCode >= 200 && res.statusCode < 300,
-              status: res.statusCode,
-              json: () => Promise.resolve(JSON.parse(data)),
-              text: () => Promise.resolve(data),
-            }));
-          });
-          
-          req.on('error', reject);
-          
-          if (init?.body) {
-            req.write(init.body);
-          }
-          req.end();
-        });
-      };
+  if (!process.env.XATA_API_KEY) {
+    throw new Error('XATA_API_KEY environment variable is not set');
+  }
 
-  instance = new XataClient({ 
+  const customFetch = typeof fetch === 'function' ? fetch : (url: string, init?: RequestInit) => {
+    const https = require('https');
+    return new Promise((resolve, reject) => {
+      const req = https.request(url, {
+        method: init?.method || 'GET',
+        headers: init?.headers,
+      }, (res) => {
+        let data = '';
+        res.on('data', chunk => data += chunk);
+        res.on('end', () => resolve({
+          ok: res.statusCode >= 200 && res.statusCode < 300,
+          status: res.statusCode,
+          json: () => Promise.resolve(JSON.parse(data)),
+          text: () => Promise.resolve(data),
+        }));
+      });
+      
+      req.on('error', reject);
+      if (init?.body) req.write(init.body);
+      req.end();
+    });
+  };
+
+  instance = new XataClient({
+    apiKey: process.env.XATA_API_KEY,
+    branch: process.env.XATA_BRANCH || 'main',
     fetch: customFetch as any
   });
   
